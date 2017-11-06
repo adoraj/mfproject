0d59f383b0ad21779ff1775b3235ffba596f662a
root <root@localhost.localdomain>
2017-11-06 10:03:33 -0500

    first commit

---
+++ _admin/data/config.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'password' => 'zx102030',
+  'host' => 'http://www.meetav.com/',
+  'replaceDomain' => '0',
+  'relativeHTML' => '0',
+  'relativeCSS' => '0',
+  'static' => '1',
+  'diyStatic' => 'css|js|html|png|jpg',
+  'cookies' => '0',
+  'agent' => '0',
+  'referer' => '0',
+  'ip' => '0',
+  'diyAgent' => '',
+  'replaces' => 
+  array (
+    0 => 
+    array (
+      'name' => 'http://192.168.35.227',
+      'seach' => 'http://192.168.35.227',
+      'replace' => 'https://192.168.35.227',
+    ),
+    1 => 
+    array (
+      'name' => '111',
+      'seach' => '<a href=\\"javascript:void[\\\\s\\\\S]*Login',
+      'replace' => '<a href=\\"javascript:void(0)\\">
+Loginasedfasdf',
+    ),
+  ),
+  'pages' => 
+  array (
+  ),
+);
